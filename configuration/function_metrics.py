321fa6d62a744cde588d9c780b6a64b671b906d0
arafferty <arafferty@carleton.edu>
2024-08-07 10:43:56 -0500

    function metric for counting tool calls at any level

--- configuration/function_metrics.py
+++ configuration/function_metrics.py
@@ -198,6 +198,30 @@ def count_llm_models(thread: Thread) -> dict:
         results["model"] = results.get("model", 0) + 1
     return results
 
+def count_tool_calls_by_name(object: Union[Thread, Turn, Message, ToolCall]) -> list:
+    # Extract ToolCall objects based on the type of object being passed in
+    toolcalls = []
+    if isinstance(object, (Thread, Turn)):
+        for message in object.messages:
+            toolcalls += [toolcall for toolcall in message.toolcalls]
+    elif isinstance(object, Message):
+        toolcalls += [toolcall for toolcall in object.toolcalls]
+    else: # Must be just a tool call
+        toolcalls.append(object)
+    
+    # Count the toolcalls
+    toolcall_counts = {}
+    for toolcall in toolcalls:
+        if toolcall.function_name not in toolcall_counts:
+            toolcall_counts[toolcall.function_name] = 0
+        toolcall_counts[toolcall.function_name] = toolcall_counts[toolcall.function_name] + 1
+    
+    # Convert to a list of name: value dictionaries
+    results = []
+    for toolcall_name, toolcall_count in toolcall_counts.items():
+        results.append({"name": toolcall_name, "value": toolcall_count})
+    return results
+
 
 def count_messages_per_role(object: Union[Thread, Turn]) -> list:
     """
