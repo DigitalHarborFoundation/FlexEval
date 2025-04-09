8c31cab7bd33a5390754209634387aea0339440d
Zachary Levonian <zach@digitalharbor.org>
2025-04-08 17:12:34 -0700

    Create new unittest from the functional tests

--- tests/unit/mixins.py
+++ tests/unit/mixins.py
@@ -1,4 +1,5 @@
 import dotenv
+import unittest.mock
 
 
 class DotenvMixin:
@@ -6,6 +7,17 @@ class DotenvMixin:
         super().setUp()
         # TODO we could restore os.environ after running this
         dotenv.load_dotenv("tests/resources/unittest.env", override=True)
+
+    def tearDown(self):
+        super().tearDown()
+
+
+class PatchOpenAIMixin:
+    def setUp(self):
+        super().setUp()
+        patcher = unittest.mock.patch("")
+        self.mock_create = patcher.start()
+        self.addCleanup(patcher.stop)
 
     def tearDown(self):
         super().tearDown()
